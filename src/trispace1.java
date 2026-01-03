479fda7817dec1d16b828d741638e7fdb5744481
A. Harish <harishvaidhesh17@gmail.com>
2026-01-04 02:38:12 +0530

    day 5 - diamond space inside a square pattern *

---
+++ src/trispace1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class trispace1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print : ");
+        int n=sc.nextInt();
+        for(int row=1;row<=n*2;row++){
+            int rowchange = row>n?(n*2)-(row-1):row;
+            for(int col =1;col<=n*2;col++){
+                if(col<=n-(rowchange-1)||col>(n+rowchange-1)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        System.out.println();
+        }
+    }
+}
+
+           
