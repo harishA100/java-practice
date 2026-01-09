283cb86203fc1f90059f46aec749122d295c333a
A. Harish <harishvaidhesh17@gmail.com>
2026-01-09 22:06:31 +0530

    day 9 - Binary Exponentation

---
+++ src/week2/biexpo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class biexpo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int exp = sc.nextInt();
+        int ans= 1;
+        while(exp>0){
+            if(exp%2!=0){
+                ans *=base;
+            }
+            
+                base *=base;
+                exp=exp/2;
+        }
+        System.out.print("The Answer is : " + ans);
+    }
+}
