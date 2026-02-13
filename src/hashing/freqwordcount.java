3c714c66d86f2184030f2a4e051535d581f20bd0
A. Harish <harishvaidhesh17@gmail.com>
2026-02-13 22:36:37 +0530

    hashing  3 - freq word count

--- src/hashing/freqwordcount.java
+++ src/hashing/freqwordcount.java
@@ -11,5 +11,6 @@ class freqwordcount{
         for(Map.Entry<String , Integer> entry : map.entrySet()){
             System.out.println(entry.getKey() + " -> "+entry.getValue());
         }
+    
     }
 }
