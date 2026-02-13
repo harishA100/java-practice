f81aa4db3273880a87a23964675514d786fa90b4
A. Harish <harishvaidhesh17@gmail.com>
2026-02-13 22:35:21 +0530

    hashinh 3 - freq word count

---
+++ src/hashing/freqwordcount.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class freqwordcount{
+    public static void main(String[] args){
+        String str1 ="I love coding I love java";
+        String str = str1.toLowerCase();
+        HashMap<String,Integer> map = new HashMap<>();
+         String[] words = str.split(" ");
+        for(String word : words){
+            map.put(word, map.getOrDefault(word,0)+1);
+        }
+        for(Map.Entry<String , Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " -> "+entry.getValue());
+        }
+    }
+}
