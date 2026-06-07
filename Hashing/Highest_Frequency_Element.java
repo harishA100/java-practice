58798428702be41f5d40daefc36f5462bd48a69a
A. Harish <harishvaidhesh17@gmail.com>
2026-06-07 22:46:37 +0530

    Highest Frequency Element

---
+++ Hashing/Highest_Frequency_Element.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Highest_Frequency_Element{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num :arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int maxfreq = 0;
+        int answer = -1;
+        for(Map.Entry<Integer,Integer> entry =map.entrySet()){
+            if(entry.getValue()>maxfreq){
+                maxfreq= entry.getValue();
+                answer= entry.getKey();
+            }
+        }
+        System.out.print(answer + " -> " + maxfreq);
+    }
+}
